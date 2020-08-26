d6ceb4ecdae67b014de190bbf71714dfac4b09bb
komarem1@seznam.cz <komarem1@seznam.cz>
2020-08-26 22:17:55 +0200

    homePageId changed to docIntro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'doc1',
+          homePageId: 'docIntro',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
